86fd70e246dffa9f63835e3cd9c98d464a8dd69a
dimi11433 <dimitricromarty@gmail.com>
2026-03-13 21:58:39 -0400

    Fixed LUT-bf16

--- FFN_SV/My_Impl/Stage1/bf16-LUT.sv
+++ FFN_SV/My_Impl/Stage1/bf16-LUT.sv
@@ -1,26 +1,26 @@
 module bf16_LUT(
     input [15:0] data_in,
-    ouput [3:0] data_out
+    ouput logic [4:0] data_out
 
 );
 
     always_comb begin
+        data_out = 0;
         if (data_in[15])begin
-           data_out = 0;
-           unique case(data_in)
-            (data_in > 16'hC080) : data_out = 0;
-            (data_in > 16'hC060) : data_out = 1;
-            (data_in > 16'hC040) : data_out = 2;
-            (data_in > 16'hC020) : data_out = 3;
-            (data_in > 16'hC000) : data_out = 4;
-            (data_in > 16'hBFC0) : data_out = 5;
-            (data_in > 16'hBF80) : data_out = 6;
-            default : data_out = 7;
+           unique case(1)
+            (data_in > 16'hC080) : data_out = 0; //<-4.0
+            (data_in > 16'hC060) : data_out = 1; //<-3.5
+            (data_in > 16'hC040) : data_out = 2; //<-3.0
+            (data_in > 16'hC020) : data_out = 3; //<-2.5
+            (data_in > 16'hC000) : data_out = 4; //<-2.0
+            (data_in > 16'hBFC0) : data_out = 5; //<-1.5
+            (data_in > 16'hBF80) : data_out = 6; //<-1.0
+            default : data_out = 7; // <-0.5
            endcase 
         end 
         else begin
-            data_out = 0;
-            unique case(data_in)
+            
+            unique case(1)
             (data_in < 16'h3F00) : data_out = 8;
             (data_in < 16'h3F80) : data_out = 9;
             (data_in < 16'h3FC0) : data_out = 10;
--- FFN_SV/My_Impl/Stage1/st1.sv
+++ FFN_SV/My_Impl/Stage1/st1.sv
@@ -1,8 +1,12 @@
 module gelu_pwl(
-
-
+    input clk,
+    input rst_n,
+    input [15:0] data_in,
+    ouput logic [15:0] data_out
 
 );
+
+    
 
 
 
