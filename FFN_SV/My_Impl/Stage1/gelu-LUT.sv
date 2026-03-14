36c2355e138aa5fb94d32a9afd978135f6f8454d
dimi11433 <dimitricromarty@gmail.com>
2026-03-14 00:25:03 -0400

    Single Accum value module done-need to do 8 instantiations in parallel now

---
+++ FFN_SV/My_Impl/Stage1/gelu-LUT.sv
@@ -0,0 +1,18 @@
+module gelu_LUT(
+    input [4:0] index_in,
+    output logic [15:0] data_out
+
+);
+
+    always_comb begin
+        case(index_in)
+            0: 
+        endcase 
+
+    end 
+
+
+
+
+
+endmodule;
