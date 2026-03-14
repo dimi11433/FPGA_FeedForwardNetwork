270dabc45abb01bb2413a9936dc1d588f2da1e9f
dimi11433 <dimitricromarty@gmail.com>
2026-03-14 16:27:35 -0400

    Fixed and Tested

--- FFN_SV/My_Impl/MatMul/mac_8cyc.sv
+++ FFN_SV/My_Impl/MatMul/mac_8cyc.sv
@@ -9,35 +9,37 @@ module mac_8cyc(
 );
 
     logic [31:0] intermediate_out;
-    logic [31:0] W1_fp32, x_fp32, b_fp32;
+    //logic [31:0] W1_fp32, x_fp32, b_fp32;
     logic [4:0] cycle_count;
+    //logic [15:0] b_reg;
     
-    assign W1_fp32 = {data_in_a, 16'h0000};
-    assign x_fp32 = {data_in_b, 16'h0000};
-    assign b_fp32 = {data_in_c, 16'h0000};
+    // assign W1_fp32 = {data_in_a, 16'h0000};
+    // assign x_fp32 = {data_in_b, 16'h0000};
+    // assign b_fp32 = {data_in_c, 16'h0000};
 
     always_ff @(posedge clk) begin
         if(!rst_n)begin
             ready <= 1'b0;
             data_out <= 16'h0000;
             intermediate_out <= 32'h00000000;
-            cycle_count <= 3'h0;
+            cycle_count <= 3'b000;
         end 
         else begin 
             if(cycle_count < 4'b1000)begin
                 cycle_count <= cycle_count + 1;
-                intermediate_out <=  intermediate_out + (W1_fp32 * x_fp32) ;
+                intermediate_out <=  intermediate_out + (data_in_a * data_in_b) ;
             end 
             else if(cycle_count == 4'b1000) begin
                 cycle_count <= cycle_count + 1;
-                intermediate_out <= intermediate_out + b_fp32;
+                intermediate_out <= intermediate_out + data_in_c;
             end 
             else begin
                 ready <= 1'b1;
+                cycle_count <= 3'b000;
                 data_out <= intermediate_out[31:16];
             end 
 
         end 
     end 
 
-endmodule; 
+endmodule
--- FFN_SV/My_Impl/MatMul/tb_mac_8yc.sv
+++ FFN_SV/My_Impl/MatMul/tb_mac_8yc.sv
@@ -1,44 +1,62 @@
 class mac_packet;
-
     rand bit [15:0] t_a;
     rand bit[15:0] t_b;
     rand bit[15:0] t_c;
+endclass
+
+class mac_model;
+
+
+    bit [3:0] counter;
+    bit [15:0] t_a;
+    bit [15:0] t_b;
+    bit [15:0] t_c;
     bit rest_n;
     bit [15:0] t_data_out;
-    bit [31:0] intermediate_out;
-    bit [31:0] w1;
-    bit [31:0] x;
-    bit [31:0] b;
+    bit [31:0] intermediate_out = 0;
+    // bit [31:0] w1 = 0;
+    // bit [31:0] x = 0;
+    // bit [31:0] b = 0;
 
-    // function new(bit rst_n = 0);
-    //     this.rest_n = rst_n;
-    // endfunction
+    function void reset();
+        counter = 0;
+        intermediate_out = 0;
+        t_data_out = 0;
+    endfunction
+
     
-    function void mac();
+    function void mac(bit [15:0] a_in, bit [15:0] b_in, bit [15:0] c_in);
+        this.t_a = a_in;
+        this.t_b = b_in;
+        this.t_c = c_in;
         if(!rest_n) begin
             intermediate_out = 32'h00000000;
             t_data_out = 16'h0000;
+            counter = 0;
         end 
         else begin
-            intermediate_out = 0;
-            for(int i = 0; i < 8; i++)begin
+            if(counter < 8)begin
+                counter = counter + 1;
+                // w1 = {t_a, 16'h0000};
+                // x = {t_b, 16'h0000};
+                intermediate_out +=  (t_a * t_b);
+            end 
+            else if(counter == 8)begin
+                counter = counter + 1;
+                //b = {t_c, 16'h0000};
+                intermediate_out = intermediate_out + t_c;
+            end 
+            else begin
+                t_data_out = intermediate_out[31:16];
+            end 
 
-                assert(this.randomize());
-                w1 = {t_a, 16'h0000};
-                x = {t_b, 16'h0000};
-                intermediate_out +=  (w1 * x);
-            end
-            b = {t_c, 16'h0000};
-            t_data_out = intermediate_out + b;
+            
         end 
 
     endfunction
 
     function void display();
-        $display("Class @%0t: t_a = %0d, t_b = %0d, t_c = %0d, t_data_out = %0d", $time, t_a, t_b, t_c, t_data_out);
-    endfunction
-    function void display_all();
-        $display("Class @%0t: w1 = %0d, x = %0d, b = %0d, t_data_out = %0d, intermediate_out = %0d", $time, w1, x, b, t_data_out, intermediate_out);
+        $display("Class @%0t: t_a = %0d, t_b = %0d, t_c = %0d, t_data_out = %0d, intermediate_out = %0d", $time, t_a, t_b, t_c, t_data_out, intermediate_out);
     endfunction
 
    
@@ -46,7 +64,8 @@ endclass
 
 
 module tb_mac_8yc;
-    mac_packet packet;
+    mac_model packet;
+    mac_packet driver;
     logic clk;
     logic rst_n;
     logic [15:0] data_in_a;
@@ -57,7 +76,7 @@ module tb_mac_8yc;
 
     mac_8cyc dut(.clk(clk), .rst_n(rst_n), .data_in_a(data_in_a), .data_in_b(data_in_b), .data_in_c(data_in_c), .data_out(data_out), .ready(ready));
 
-    inital begin
+    initial begin
         clk = 0;
         forever #5 clk = ~clk;
     end
@@ -66,33 +85,44 @@ module tb_mac_8yc;
         $dumpfile("tb_mac_8yc.vcd");
         $dumpvars(0, tb_mac_8yc);
         packet = new();
-        rst_n = 0;
-        packet.rest_n = rst_n;
-        packet.mac();
+        driver = new();
 
-        data_in_a = packet.t_a;
-        data_in_b = packet.t_b;
-        data_in_c = packet.t_c;
-    
+        rst_n = 0;
+        @(posedge clk);
+        packet.rest_n = rst_n;
+        packet.reset();
+        #5;
         rst_n = 1;
         packet.rest_n = rst_n;
 
-        always@(posedge clk);
-        packet.mac();
-        #10;
-        packet.display_all();
+        repeat(10)begin
+            assert(driver.randomize());
+            
+            data_in_a = driver.t_a;
+            data_in_b = driver.t_b;
+            data_in_c = driver.t_c;
+            
+            
+
+            driver.t_c.rand_mode(0);
+            @(posedge clk);
+            packet.mac(driver.t_a, driver.t_b, driver.t_c);
+            #10;
+            packet.display();
+            //packet.display_all();
+            $display("Testbench @%0t: data_in_a = %0d, data_in_b = %0d, data_in_c = %0d, data_out = %0d", $time, data_in_a, data_in_b, data_in_c, data_out);
+             
+        end 
         if(packet.t_data_out == data_out)begin
-            $display("Test passed");
+                $display("Test passed");
         end 
         else begin
             $display("Test failed");
         end 
         $finish;
+
+        
     end 
     
-
-
-
-
 
 endmodule 
